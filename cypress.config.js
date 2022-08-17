750435f8c98b716df4621173fbe35a82cd8b8250
Rachel <Raquel.rudermano@gmail.com>
2022-08-17 15:54:41 -0700

    Add okta boilerplate code

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {},
-  },
-}
+const { defineConfig } = require('cypress')
+
+// Populate process.env with values from .env file
+require('dotenv').config()
+
+module.exports = defineConfig({
+  env: {
+    auth_username: process.env.AUTH_USERNAME,
+    auth_password: process.env.AUTH_PASSWORD,
+    okta_domain: process.env.REACT_APP_OKTA_DOMAIN,
+    okta_client_id: process.env.REACT_APP_OKTA_CLIENTID
+  }
+})
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,13 @@
-/// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('Okta', function () {
+  beforeEach(function () {
+    cy.task('db:seed')
+    cy.loginByOktaApi(
+      Cypress.env('auth_username'),
+      Cypress.env('auth_password')
+    )
+  })
+
+  it('shows onboarding', function () {
+    cy.contains('Get Started').should('be.visible')
   })
 })
